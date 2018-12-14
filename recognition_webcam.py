cfee430be76d314b1b6b9b92e3e33a030101a9c5
Spidy20 <kushalbhavsar58@gmail.com>
2018-12-14 13:22:01 +0530

    New commit

--- recognition_webcam.py
+++ recognition_webcam.py
@@ -22,17 +22,31 @@ while True:
     # Draw rectangles around each face
     for f in faces:
         (x, y, w, h) = [v * size for v in f]  # Scale the shapesize backup
-        cv2.rectangle(im, (x, y), (x + w, y + h), (255, 255 ,25), 7)
-        # Save just the rectangle faces in SubRecFaces
         sub_face = im[y:y + h, x:x + w]
         FaceFileName = "test.jpg"  # Saving the current image from the webcam for testing.
         cv2.imwrite(FaceFileName, sub_face)
         text = label_image.main(FaceFileName)  # Getting the Result from the label_image file, i.e., Classification Result.
         text = text.title()  # Title Case looks Stunning.
         font = cv2.FONT_HERSHEY_TRIPLEX
-        cv2.putText(im, text, (x + w, y), font, 1, (0, 25, 255), 2)
+
+        if text == 'Angry':
+            cv2.rectangle(im, (x, y), (x + w, y + h), (0, 25, 255), 7)
+            cv2.putText(im, text, (x + h, y), font, 1, (0, 25,255), 2)
+
+        if text == 'Smile':
+            cv2.rectangle(im, (x, y), (x + w, y + h), (0,260,0), 7)
+            cv2.putText(im, text, (x + h, y), font, 1, (0,260,0), 2)
+
+        if text == 'Fear':
+            cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 255), 7)
+            cv2.putText(im, text, (x + h, y), font, 1, (0, 255, 255), 2)
+
+        if text == 'Sad':
+            cv2.rectangle(im, (x, y), (x + w, y + h), (0,191,255), 7)
+            cv2.putText(im, text, (x + h, y), font, 1, (0,191,255), 2)
+
     # Show the image/
-    cv2.imshow('Emotion recognition system', im)
+    cv2.imshow('Emotion recognition', im)
     key = cv2.waitKey(30)& 0xff
 
     if key == 27:  # The Esc key
